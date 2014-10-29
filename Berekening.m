64d630d25efb1ec6a5bd5529f12ccafe3389256d
bjfh <bramishier2@hotmail.com>
2014-10-29 18:21:22 +0100

    Update Berekening.m

--- Berekening.m
+++ Berekening.m
@@ -1,7 +1,26 @@
 while ((AfstandY(n-1) > 0) && (n <= nmax))
+    Snelheid = sqrt(SnelheidX(n-1)^2 + SnelheidY(n-1)^2);
+    if (SnelheidX(n-1) ~= 0)
+        BewegingsHoekXAs = atand(SnelheidY(n-1)/SnelheidX(n-1));
+    elseif (SnelheidY >= 0)
+        BewegingsHoekXAs = 90;
+    else
+        BewegingsHoekXAs = -90;
+    end
+    AanvalsHoek = AanvalsHoekXAs - BewegingsHoekXAs;
+    DragCoef = CD0 + CDalpha * ((AanvalsHoek - Alpha0) * pi / 180)^2;
+    Drag = -DichtheidLucht * (Snelheid ^ 2) * OppervlakFrisbee *DragCoef / 2;
+    LiftCoef = CL0 + CLalpha * AanvalsHoek * pi / 180;
+    Lift = DichtheidLucht * (Snelheid ^ 2) * OppervlakFrisbee *LiftCoef / 2;
+        
     %X as
-    Drag = -(WeerstandCoef * DichtheidLucht * OppervlakFrisbee * (SnelheidX(n-1)^2)) / 2;
-    XKrachten = Drag;
+    if (SnelheidX(n-1) ~= 0)
+        DragX = SnelheidX(n-1)/abs(SnelheidX(n-1)) * Drag * cosd(BewegingsHoekXAs);
+    else
+        DragX = 0;
+    end
+    LiftX = Lift * sind(BewegingsHoekXAs);
+    XKrachten = DragX + LiftX;
     AcceleratieX = XKrachten / Massa;
     DeltaSnelheidX = AcceleratieX * DeltaTijd;
     SnelheidX(n) = SnelheidX(n-1) + DeltaSnelheidX;
@@ -9,9 +28,14 @@ while ((AfstandY(n-1) > 0) && (n <= nmax))
     AfstandX(n) = AfstandX(n-1) + DeltaAfstandX;
     
     %Y as
-    LiftKracht = (LiftCoef * DichtheidLucht * OppervlakFrisbee * (SnelheidX(n-1)^2))/2;
-    ZwaarteKracht = Massa * Gravitatie;
-    YKrachten = LiftKracht + ZwaarteKracht;
+    if (SnelheidY(n-1) ~= 0)
+        DragY = SnelheidY(n-1)/abs(SnelheidY(n-1)) * Drag * sind(BewegingsHoekXAs);
+    else
+        DragY = 0;
+    end
+    LiftY = -Lift * cosd(BewegingsHoekXAs);
+    ZwaarteKracht = Gravitatie * Massa;
+    YKrachten = ZwaarteKracht + LiftY + DragY;
     AcceleratieY = YKrachten / Massa;
     DeltaSnelheidY = AcceleratieY * DeltaTijd;
     SnelheidY(n) = SnelheidY(n-1) + DeltaSnelheidY;
@@ -20,10 +44,5 @@ while ((AfstandY(n-1) > 0) && (n <= nmax))
     
     %Tijd en iteratie
     Tijd(n) = Tijd(n-1) + DeltaTijd;
-    if rem(n, 10) == 0
-        n
-        DeltaSnelheidX
-        DeltaSnelheidY
-    end
     n = n + 1;
 end
