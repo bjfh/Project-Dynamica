c1d1354628a5df0fdbf7066c19b08dec736a03b1
bjfh <bramishier2@hotmail.com>
2014-10-29 18:21:52 +0100

    Update Initiele_Waarden.m

--- Initiele_Waarden.m
+++ Initiele_Waarden.m
@@ -2,11 +2,11 @@
 n = 1;                                              %iteratie nummer
 nmax = 10000;                                       %maximaal aantal itteraties
 Massa = 0.175;                                      %kg
+CL0 = 0.15;                                         %1
+CLalpha = 1.4;                                      %1
 CD0 = 0.08;                                         %1
 CDalpha = 2.72;                                     %1
 Alpha0 = -4;                                        %graden
-CL0 = 0.15;                                         %1
-CLalpha = 1.4;                                      %1
 Tijd(n) = 0;                                        %s
 DeltaTijd = 0.001;                                  %s
 
@@ -20,23 +20,21 @@ Tijd = zeros(1,nmax);
 
 %Beide dimensies
 Snelheid = 14;                                      %m*s^-1
-AanvalsHoekXAs = 7.5;                                 %graden
+BewegingsHoekXAs = 45;                               %graden
+AanvalsHoekXAs = 50;                                %graden
 DichtheidLucht = 1.23;                              %kg/m^3
-DiameterFrisbee = 0.28;                              %m
+DiameterFrisbee = 0.268924;                         %m
 OppervlakFrisbee = 1/4 * pi * DiameterFrisbee^2;    %m^2
 
-
 %Dimensie X
-WeerstandCoef = CD0 + CDalpha * ((AanvalsHoekXAs - Alpha0) * pi / 180)^2;%1
 AfstandX(n) = 0;                                    %m
 DeltaAfstandX = 0;
-SnelheidX(n) = Snelheid;     %m*s^-1
+SnelheidX(n) = Snelheid * cosd(BewegingsHoekXAs);   %m*s^-1
 
 %Dimensie Y
-LiftCoef = CL0 + CLalpha * AanvalsHoekXAs * pi / 180;%1
 AfstandY(n) = 1;                                    %m
 DeltaAfstandY = 0;
-SnelheidY(n) = 0;     %m*s^-1
+SnelheidY(n) = Snelheid * sind(BewegingsHoekXAs);   %m*s^-1
 Gravitatie = -9.81;                                 %m*s^-2
 
 %Begin Waarden zijn gegeven
